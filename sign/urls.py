8002f5fb94aa471c0abd33167b66b03397cafd95
Robert <robert.plekhanov@bk.ru>
2023-12-22 15:40:08 +0300

    fix: Исправлена ошибка невозможности выхода из системы

--- sign/urls.py
+++ sign/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
+from .views import logout_view
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="sign/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(template_name="sign/logout.html"), name="logout"),
+    path("logout/", logout_view, name="logout"),
 ]
